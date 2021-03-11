0c8c1ab9b2e08c380ea8c48cee08b649ab46a426
gogi999 <g.petrovic9@gmail>
2021-03-11 15:34:22 +0100

    Updated file(s) in ex07/src folder

--- ex07/src/index.js
+++ ex07/src/index.js
@@ -1,14 +1,16 @@
 function main(numOne, numTwo){
     // Only change code below this line
     var reminder;
-    numOne = parseInt(numOne);
-    numTwo = parseInt(numTwo);
+    var numOne = parseInt(numOne);
+    var numTwo = parseInt(numTwo);
 
     reminder = numOne % numTwo;
     // Only change code above this line
     return reminder;
 }
 
-console.log(main(102, 25)); // Change this line
+console.log(main(5, 2)); // Change this line
+console.log(main(15, 4));
+console.log(main(102, 25));
 
 module.exports = main;
