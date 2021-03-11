7306f7c1852e88f93e488c287916dd38c65d885a
gogi999 <g.petrovic9@gmail>
2021-03-11 12:59:44 +0100

    Updated file(s) in ex05/src folder

--- ex05/src/index.js
+++ ex05/src/index.js
@@ -1,5 +1,7 @@
 function main(numOne, numTwo){
     // Only change code below this line
+    numOne = parseFloat(numOne);
+    numTwo = parseFloat(numTwo);
     var product = parseFloat(numOne * numTwo);
     // Only change code above this line
     return product; 
