b6eb44d20af2c5d3b27ea05de8429f45343ab666
gogi999 <g.petrovic9@gmail>
2021-03-11 15:10:25 +0100

    Updated file(s) in ex01/src folder

--- ex01/src/index.js
+++ ex01/src/index.js
@@ -4,6 +4,8 @@ function main(numOne, numTwo){
         quotient = 'Divide by 0! Cannot divide by zero!';
     } else {
         // Only change code below this line
+        var numOne;
+        var numTwo;
         quotient = numOne / numTwo;
         // Only change code above this line
     }
@@ -11,6 +13,9 @@ function main(numOne, numTwo){
     return quotient;
 }
 
-console.log(main(55, 0)); // change this line
+console.log(main(15, 3)); // change this line
+console.log(main(25, 5));
+console.log(main(420, 10));
+console.log(main(55, 0));
 
 module.exports = main;
