146f6ceee4e3214fcb8550f78afb020c61b68283
gogi999 <g.petrovic9@gmail>
2021-03-11 13:49:41 +0100

    Added file(s) in ex10/src folder

---
+++ ex10/src/index.js
@@ -0,0 +1,16 @@
+function main(){
+    // Only change code below this line
+    var myFirstName = "Goran";
+    var myLastName = 'Petrovic';
+    var myHomeTown = "Mostar";
+    // Only change code above this line
+    return {
+        myFirstName,
+        myLastName,
+        myHomeTown
+    };
+}   
+
+console.log(main());
+
+module.exports = main;
