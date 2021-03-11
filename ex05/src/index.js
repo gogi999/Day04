259f05baed5a677633160aeb661677c24a6b817c
gogi999 <g.petrovic9@gmail>
2021-03-11 12:41:25 +0100

    Added file(s) in ex05/src folder

---
+++ ex05/src/index.js
@@ -0,0 +1,10 @@
+function main(numOne, numTwo){
+    // Only change code below this line
+    var product = parseFloat(numOne * numTwo);
+    // Only change code above this line
+    return product; 
+}
+
+console.log(main(100.25, 43.20)); // change this line
+
+module.exports = main;
