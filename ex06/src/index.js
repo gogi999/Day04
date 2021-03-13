c23ae15e4cfe3826c46e44e2cc3b8ba59331d798
gogi999 <g.petrovic9@gmail>
2021-03-13 10:42:18 +0100

    Updated file(s) in ex06/src folder

--- ex06/src/index.js
+++ ex06/src/index.js
@@ -1,10 +1,10 @@
 function main(numOne, numTwo){
     // Only change code below this line
-    var numOne = parseFloat(numOne).toFixed(1);
-    var numTwo = parseFloat(numTwo).toFixed(1);
-    var myQuotient = parseFloat(numOne / numTwo).toFixed(1);
+    numOne = parseFloat(numOne);
+    numTwo = parseFloat(numTwo);
+    var product = parseFloat(numOne / numTwo);
     // Only change code above this line
-    return myQuotient;
+    return product; 
 }
 
 console.log(main(5.0, 2.0)); // change this line
